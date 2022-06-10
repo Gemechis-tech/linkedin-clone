6b49eb9b2a9b3a8e57f97e20177ef14cbe022ae0
Gemechis Shiferaw <gemechisshiferawk@gmail.com>
2022-06-10 15:14:30 +0300

    Header Part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-     <h1>Gemechis Shiferaw Linkedin clone so special sdfsdfdfdfsdssssssssss</h1>
+    <Header />
+
     </div>
   );
 }
