872a854117df66aecd99c849d341358caa378aa5
Gemechis Shiferaw <gemechisshiferawk@gmail.com>
2022-06-10 12:01:41 +0300

    cleaning

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>Gemechis Shiferaw Linkedin clone so special sdfsdfdfdfsdssssssssss
-       
-     </h1>
+     <h1>Gemechis Shiferaw Linkedin clone so special sdfsdfdfdfsdssssssssss</h1>
     </div>
   );
 }
