fd894f2011e09a81e13b2bc20f9770237ced8576
Gemechis Shiferaw <gemechisshiferawk@gmail.com>
2022-06-10 10:05:25 +0300

    New commitd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <h1>Gemechis Shiferaw Linkedin clone so special
+     <h1>Gemechis Shiferaw Linkedin clone so special sdfsdfdfdfsdssssssssss
        
      </h1>
     </div>
