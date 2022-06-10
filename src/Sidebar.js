c144776d9b4a908318e1ac2eb788b0ee261c84bb
Gemechis Shiferaw <gemechisshiferawk@gmail.com>
2022-06-10 15:26:17 +0300

    sidebar idea

---
+++ src/Sidebar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Sidebar.css'
+import {Avatar} from "@material-ui/core";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+        <div className='sidebar__top'>
+        <img src="" alt=""/>
+        <Avatar className='sidebar__avatar' />
+        <h2>Gemechis Shiferaw</h2>
+        <h4>gemechisshiferawk@gmail.com</h4>
+
+        </div>
+       
+
+    </div>
+  )
+}
+
+export default Sidebar
