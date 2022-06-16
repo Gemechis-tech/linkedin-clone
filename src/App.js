3cc2bef9608cd4a1ac89db3e39973c09f7a14995
Gemechis Shiferaw <gemechisshiferawk@gmail.com>
2022-06-16 19:18:27 +0300

    First Login

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import { initializeUseSelector } from 'react-redux/es/hooks/useSelector';
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 import Feed from './Feed';
+import Login from './Login';
+import { selectUser } from './features/userSlice';
 
 function App() {
+
+ const user = initializeUseSelector(selectUser);
   return (
     <div className="app">
     <Header />
-
-    <div className='app__body'>
+    {!user ? (<Login />) :
+    (
+      <div className='app__body'>
       <Sidebar />
-      <Feed />
+      <Feed  />
     </div>
+
+    )}
+  
 
     </div>
   );
